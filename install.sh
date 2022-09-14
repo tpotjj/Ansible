b20ad4b3d94f0b893677a86f5f446fcb581503c8
Joris Jansen <j.jansen@ispnext.com>
2022-09-14 05:39:03 +0200

    Remove temp Ansible repo from install script

--- install.sh
+++ install.sh
@@ -5,7 +5,4 @@ sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 
-## Pull Ansible repo
-# mkdir Ansible-temp
-# cd Ansible-temp
 sudo ansible-pull -U https://github.com/tpotjj/Ansible.git
