45ea7077b3ec4c0d00509341f761778741f800d7
Joris Jansen <j.jansen@ispnext.com>
2022-09-16 05:27:06 +0200

    Remove ansible-pull from install script

--- install.sh
+++ install.sh
@@ -4,5 +4,3 @@
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
-
-sudo ansible-pull -U https://github.com/tpotjj/Ansible.git
